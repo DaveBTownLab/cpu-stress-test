47ae0dfe674144d9b800fd259ebe32d3525cdcca
Dave <davesaekert@web.de>
2026-09-13 21:52:29 +0200

    Fix script path detection

--- stresstest.sh
+++ stresstest.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
-SCRIPT_DIR="$(cd "$dirname"$0")" %% pwd)"
+SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 CPU_SCRIPT="$SCRIPT_DIR/scripts/stresscpu.sh"
-MONITOR_SCRIPT="$SCRIPT_DIR/scripts/stressmon.sh
+MONITOR_SCRIPT="$SCRIPT_DIR/scripts/stressmon.sh"
 
 DURATION=${1:-60}
+
+echo "$SCRIPT_DIR"
+echo "$CPU_SCRIPT"
+echo "$MONITOR_SCRIPT"
 
 
 xfce4-terminal \
